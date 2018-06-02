1ac4ed2fa344c50b20a81e4c669a05e3c36cf1d8
Bram Inniger <bram@inniger.be>
2018-06-03 00:26:08 +0300

    Solved the fourth problem and added a Palindrome tester

---
+++ src/test/kotlin/problems01to10/Problem04Test.kt
@@ -0,0 +1,12 @@
+package problems01to10
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Problem04Test {
+
+    @Test
+    fun verifySolution() {
+        assertEquals(906609, solve04())
+    }
+}
---
+++ src/test/kotlin/util/PalindromeTest.kt
@@ -0,0 +1,37 @@
+package util
+
+import org.junit.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+
+class PalindromeTest {
+
+    @Test
+    fun `Empty and single element lists are palindrome`() {
+        assertTrue(isPalindrome(emptyList()))
+        assertTrue(isPalindrome(listOf(42)))
+        assertTrue(isPalindrome(listOf('a')))
+        assertTrue(isPalindrome(listOf("b")))
+    }
+
+    @Test
+    fun `Can detect palindrome lists`() {
+        assertTrue(isPalindrome(listOf("a", "a")))
+        assertTrue(isPalindrome(listOf("a", "b", "a")))
+        assertTrue(isPalindrome(listOf("a", "b", "b", "a")))
+        assertTrue(isPalindrome(listOf(42, 1, 42)))
+        assertTrue(isPalindrome(listOf('c', 'd', 'e', 'd', 'c')))
+        assertTrue(isPalindrome(listOf("b", "b", "b", "b")))
+    }
+
+    @Test
+    fun `Can detect not palindrome lists`() {
+        assertFalse(isPalindrome(listOf("a", "b")))
+        assertFalse(isPalindrome(listOf("a", "a", "b")))
+        assertFalse(isPalindrome(listOf("a", "b", "a", "b")))
+        assertFalse(isPalindrome(listOf(42, 1, 41)))
+        assertFalse(isPalindrome(listOf('c', 'e', 'd', 'd', 'c')))
+        assertFalse(isPalindrome(listOf("b", "b", "b", "a")))
+    }
+}
