b4cbef1ea54e24522caa66711152025d8e5f9dc3
Bram Inniger <bram@inniger.be>
2018-06-03 01:21:27 +0300

    Solved the sixth problem

---
+++ src/main/kotlin/problems01to10/Problem06.kt
@@ -0,0 +1,16 @@
+package problems01to10
+
+private const val MAX_VALUE = 100
+
+/**
+ * Sum square difference
+ *
+ * The sum of the squares of the first ten natural numbers is,
+ * 1^2 + 2^2 + ... + 10^2 = 385
+ * The square of the sum of the first ten natural numbers is,
+ * (1 + 2 + ... + 10)^2 = 55^2 = 3025
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
+fun solve06() = IntRange(1, MAX_VALUE).sum().let { it * it } -
+        IntRange(1, MAX_VALUE).map { it * it }.sum()
---
+++ src/test/kotlin/problems01to10/Problem06Test.kt
@@ -0,0 +1,12 @@
+package problems01to10
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Problem06Test {
+
+    @Test
+    fun verifySolution() {
+        assertEquals(25164150, solve06())
+    }
+}
