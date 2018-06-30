4f48c495b9998f8ada212d3968d11067b887fcad
Bram Inniger <bram@inniger.be>
2018-06-30 13:59:28 +0300

    Tiny cleanup

--- src/main/kotlin/util/Palindrome.kt
+++ src/main/kotlin/util/Palindrome.kt
@@ -3,7 +3,5 @@ package util
 tailrec fun isPalindrome(list: List<Any>): Boolean = when {
     list.size <= 1 -> true
     list[0] != list[list.lastIndex] -> false
-    else -> {
-        isPalindrome(list.subList(1, list.lastIndex))
-    }
+    else -> isPalindrome(list.subList(1, list.lastIndex))
 }
--- src/test/kotlin/util/PalindromeTest.kt
+++ src/test/kotlin/util/PalindromeTest.kt
@@ -4,7 +4,6 @@ import org.junit.Test
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
-
 class PalindromeTest {
 
     @Test
