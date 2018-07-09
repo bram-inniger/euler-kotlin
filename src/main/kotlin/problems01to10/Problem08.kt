ee45de5c0f8362c73135632ae94e388e240d5119
Bram Inniger <bram@inniger.be>
2018-07-09 23:29:19 +0300

    Solved the eighth problem

---
+++ src/main/kotlin/problems01to10/Problem08.kt
@@ -0,0 +1,27 @@
+package problems01to10
+
+import java.lang.Character.*
+
+private const val NR_ADJACENT_DIGITS = 13
+
+/**
+ * Largest product in a series
+ *
+ * The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+ * Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+ */
+fun solve08() = IntRange(0, number.size - NR_ADJACENT_DIGITS - 1)
+    .map { number.subList(it, it + NR_ADJACENT_DIGITS) }
+    .map { digits ->
+        digits.map(Int::toLong)
+            .reduce { acc, digit -> acc * digit }
+    }
+    .sorted()
+    .last()
+
+private val number
+    get() = Thread.currentThread().contextClassLoader.getResourceAsStream("Problem08.txt")
+        .reader()
+        .readText()
+        .filter(::isDigit)
+        .map(::getNumericValue)
---
+++ src/test/kotlin/problems01to10/Problem08Test.kt
@@ -0,0 +1,12 @@
+package problems01to10
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Problem08Test {
+
+    @Test
+    fun verifySolution() {
+        assertEquals(23514624000L, solve08())
+    }
+}
