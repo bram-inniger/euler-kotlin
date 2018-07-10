5e0953d3abe622ed158612b9c29c9f7da29fa2da
Bram Inniger <bram@inniger.be>
2018-07-10 09:45:48 +0300

    Solved the ninth problem

---
+++ src/main/kotlin/problems01to10/Problem09.kt
@@ -0,0 +1,31 @@
+package problems01to10
+
+private const val TRIPLET_SUM = 1000
+
+/**
+ * Special Pythagorean triplet
+ *
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ * a^2 + b^2 = c^2
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
+fun solve09() = (1..TRIPLET_SUM)
+    .flatMap { a ->
+        (1..TRIPLET_SUM).map { b -> PythagoreanTriplet(a, b, TRIPLET_SUM - a - b) }
+    }
+    .first(PythagoreanTriplet::valid)
+    .prod
+
+private data class PythagoreanTriplet(val a: Int, val b: Int, val c: Int) {
+
+    internal val valid
+        get() = a > 0 &&
+                b > 0 &&
+                c > 0 &&
+                a * a + b * b == c * c
+
+    internal val prod
+        get() = a * b * c
+}
---
+++ src/test/kotlin/problems01to10/Problem09Test.kt
@@ -0,0 +1,12 @@
+package problems01to10
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Problem09Test {
+
+    @Test
+    fun verifySolution() {
+        assertEquals(31875000, solve09())
+    }
+}
